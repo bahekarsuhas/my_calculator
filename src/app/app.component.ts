faceb63c96cc768a10eb4e10b58d02996af322e5
Suhas Bahekar <bahekarsuhas1984@gmail.com>
2020-12-22 00:59:36 -0600

    adding functionality to html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-calculator';
+
+  wholeCalculationStr: any [] = [];
+  currentOperatingNumber = '';
+  finalCalculation = 0;
+
+  operation(operation: string) {
+    console.log("onOperation : " + this.wholeCalculationStr);
+    console.log("last entry : " + this.wholeCalculationStr[this.wholeCalculationStr.length - 1]);
+    if (operation !== 'EQUALS'){
+      if (this.currentOperatingNumber !== ''){
+        this.wholeCalculationStr.push(this.currentOperatingNumber);
+        this.wholeCalculationStr.push(operation);
+      }
+      this.currentOperatingNumber = '';
+      console.log(this.wholeCalculationStr);
+    }else {
+      
+    }
+    
+  }
+
+  operatingNumber (selectedNumber: string){
+       this.currentOperatingNumber = this.currentOperatingNumber.concat(selectedNumber);
+  }
 }
+
+
